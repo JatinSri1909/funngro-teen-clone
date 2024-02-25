160e07e90c81d5ef21fb343f7e7868282ef68d12
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-02-25 17:18:20 +0530

    Responsiveness of Gallery component imporoved

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -48,9 +48,9 @@ const Gallery = () => {
                 <p className="text-color-grey">Lakhs of Teens love Funngro, Hundreds of Companies love Funngro and now Sharks also loved Funngro</p>
             </div>
             <Slider {...settings}>
-                <div>
-                    <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FsGysgwm02YY%3Ffeature%3Doembed&display_name=YouTube&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DsGysgwm02YY&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FsGysgwm02YY%2Fhqdefault.jpg&key=96f1f04c5f4143bcb0f2e68c87d65feb&type=text%2Fhtml&schema=youtube" width="940" height="528" scrolling="no" allowFullScreen title="Funngro @ Shark Tank"></iframe>
-                </div>
+            <div className="video-wrapper">
+  <iframe className="embedly-embed" src="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FsGysgwm02YY%3Ffeature%3Doembed&display_name=YouTube&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DsGysgwm02YY&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FsGysgwm02YY%2Fhqdefault.jpg&key=96f1f04c5f4143bcb0f2e68c87d65feb&type=text%2Fhtml&schema=youtube" scrolling="no" allowFullScreen title="Funngro @ Shark Tank"></iframe>
+</div>
                 <div>
                     <img src="https://assets-global.website-files.com/638b48215fd2fd34538fa6bc/63fdf3f03e4e4d213cda423f_Funngro%20-%20Shark%20Tank.png" alt="" className="image-full-width gallery-cover" />
                 </div>
